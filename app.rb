efc4169d632a520cc4caddec688b1b8607e97cfc
Marco Bagnasco <admin@hecticmojo.com>
2016-02-02 13:19:11 +0000

    Params introduced into the app

--- app.rb
+++ app.rb
@@ -9,7 +9,18 @@ get '/secret' do
 end
 
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].shuffle.pop
   erb(:index)
 end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age  = params[:age ]
+  erb(:index2)
+end
+
+# post '/named-cat' do
+#   @details = params[:]
+# end
